b44803217e1224e89076c392b71d4bca4457607d
shakibchakladar <shakibchakladar@gmail.com>
2024-04-30 00:35:20 +0600

    get the view details

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9ttivus.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -49,6 +49,12 @@ async function run() {
       res.send(result);
   });
   
+
+  app.get('/singleProduct/:id', async(req,res)=>{
+    const result=await craftCollection.findOne({_id:new ObjectId(req.params.id)})
+    res.send(result)
+  })
+
 
 
 
